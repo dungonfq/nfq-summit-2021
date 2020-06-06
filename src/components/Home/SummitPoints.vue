4c2879cf7a50e15c5db6b185aeac382c07ff0f5b
dungonfq <57652048+dungonfq@users.noreply.github.com>
2020-06-06 20:37:22 +0700

    Completed photos & video gallery section

--- src/components/Home/SummitPoints.vue
+++ src/components/Home/SummitPoints.vue
@@ -1,23 +1,25 @@
 <template>
-    <div class="summit-points container">
-        <div class="summit-points__header">
-            <div class="summit__headline">
-                The One-Of-A-Kind Summit
-            </div>
-            <div class="summit__desc">
-                Make meaningful connections while immersing yourself in the exotic Vietnamese scenery.
-            </div>
-        </div>
-        <div class="summit-points__main">
-            <div v-for="item in points"
-                :key="`points_${item.id}`"
-                class="summit-points__item item">
-                <div class="item__header">
-                    <span class="item__id">{{ item.id }}</span>
-                    <span class="item__title">{{ item.title }}</span>
+    <div class="summit-points">
+        <div class="container">
+            <div class="summit__header">
+                <div class="summit__headline">
+                    The One-Of-A-Kind Summit
                 </div>
-                <div class="item__desc">
-                    {{ item.value }}
+                <div class="summit__desc">
+                    Make meaningful connections while immersing yourself in the exotic Vietnamese scenery.
+                </div>
+            </div>
+            <div class="summit-points__main">
+                <div v-for="item in points"
+                    :key="`points_${item.id}`"
+                    class="summit-points__item item">
+                    <div class="item__header">
+                        <span class="item__id">{{ item.id }}</span>
+                        <span class="item__title">{{ item.title }}</span>
+                    </div>
+                    <div class="item__desc">
+                        {{ item.value }}
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,16 +3,19 @@
     <div class="home">
         <TopBg />
         <SummitPoints />
+        <LastYearMoments />
     </div>
 </template>
 
 <script>
 import TopBg from "@/components/Home/TopBg";
 import SummitPoints from "@/components/Home/SummitPoints";
+import LastYearMoments from "@/components/Home/LastYearMoments";
 export default {
     components: {
         TopBg,
-        SummitPoints
+        SummitPoints,
+        LastYearMoments
     }
 }
 </script>
