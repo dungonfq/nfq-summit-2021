f1d3f1e15c1a0a44992f95f659cdacff36e2fda8
dungonfq <57652048+dungonfq@users.noreply.github.com>
2020-06-06 14:02:49 +0700

    Init vue app

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "./pages/home/Home";
+export default {
+  name: "App",
+  components: {
+    Home
+  }
+};
+</script>
