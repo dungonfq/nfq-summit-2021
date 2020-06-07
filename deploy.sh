e44899faf06250f24023b3b094a991c1fdda1ac6
dungonfq <57652048+dungonfq@users.noreply.github.com>
2020-06-07 19:38:20 +0700

    Full screen iphone X's landscape

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:dungonfq/nfq-summit-2021.git master:gh-pages
+
+cd -
