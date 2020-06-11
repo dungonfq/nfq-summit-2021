8d9035dc3290c1960222735eca738eb053ebd08c
dungonfq <57652048+dungonfq@users.noreply.github.com>
2020-06-11 09:27:31 +0700

    Run eslint

--- src/data/s3.js
+++ src/data/s3.js
@@ -1,4 +1,3 @@
-const AWS_URL = 'https://nfq-summit.s3-ap-southeast-1.amazonaws.com/summit-2021/';
-export {
-    AWS_URL
-}
+const AWS_URL =
+  "https://nfq-summit.s3-ap-southeast-1.amazonaws.com/summit-2021/";
+export { AWS_URL };
