0ba93de6f03c152cc51d845bdd37d3091f277430
dungonfq <57652048+dungonfq@users.noreply.github.com>
2020-06-11 11:03:02 +0700

    Run lint

--- src/components/Home/TopBg.vue
+++ src/components/Home/TopBg.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="top-bg top-bg--py" :style="{ 'background-image': 'url(/images/background/cover.png)' }">
+  <div
+    class="top-bg top-bg--py"
+    :style="{ 'background-image': 'url(/images/background/cover.png)' }"
+  >
     <div class="top-bg__main">
       <div class="top-bg__headline">.NFQ ASIA SUMMIT 2021</div>
       <div class="top-bg__desc">
